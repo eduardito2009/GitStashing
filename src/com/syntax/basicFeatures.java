b87fd4fd91d245378f5ed277e3617e5f9dfe9974
eduardito2009 <cm655176@gmail.com>
2022-05-15 11:03:18 -0400

    The basic structure of the car is ready, working on advance features

---
+++ src/com/syntax/basicFeatures.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class basicFeatures {
+    String engineCapac;
+    String color;
+    String doors;
+    String headlights;
+    String regNo;
+}
---
+++ src/com/syntax/main.java
@@ -0,0 +1,9 @@
+package com.syntax;
+
+public class main {
+
+    public static void main(String[] args) {
+        //write your code
+        basicFeatures car = new basicFeatures();
+    }
+}
