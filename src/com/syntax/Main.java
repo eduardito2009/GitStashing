b97a27d52aae328466bf0eedf8bd676845317723
eduardito2009 <cm655176@gmail.com>
2022-05-15 12:05:31 -0400

    The red car is ready with some basic features

---
+++ src/com/syntax/Main.java
@@ -0,0 +1,15 @@
+package com.syntax;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //write your code
+        basicFeatures Audi = new basicFeatures();
+        Audi.color="red";
+        Audi.doors="4";
+        Audi.engineCapac="V8";
+        Audi.headlights="blue";
+        Audi.regNo="tuzik";
+
+    }
+}
--- src/com/syntax/main.java
+++
@@ -1,9 +0,0 @@
-package com.syntax;
-
-public class main {
-
-    public static void main(String[] args) {
-        //write your code
-        basicFeatures car = new basicFeatures();
-    }
-}
